36ec43f4fbfda38b6300e0e2154a6335808194a0
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-04-16 18:31:47 +0100

    solution

--- ruby20221604.rb
+++ ruby20221604.rb
@@ -1,1 +1,9 @@
-puts "hello world"
+text = "hi hi hi THIS this"
+
+def top_3_words(text)
+  freq = Hash.new(0)
+  text.split(" ").each {|word| freq[word] += 1 }
+  puts "#{freq.sort_by {|k, v| v}.map {|x| x[0]}.slice([-3, -freq.count].max..-1)}"
+end
+
+top_3_words(text)
